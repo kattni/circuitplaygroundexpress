5a56156d61690fbd0d808bccb1d4500eafd4b003
Kattni <kattni@kittyfish.org>
2017-08-02 18:36:35 -0400

    Updated for current version of CPX module

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
 import board
-import neopixel
 import time
 import digitalio
 import math
 import array
 import audioio
 import touchio
+import adafruit_circuitplayground.express
+from adafruit_circuitplayground.express import circuit
 from board import *
 
 length = 8000 // 440
@@ -16,7 +16,6 @@ for i in range(length):
 
 speaker_enable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
 speaker_enable.switch_to_output(value=True)
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
 touch1 = touchio.TouchIn(A1)
 touch2 = touchio.TouchIn(A2)
 touch3 = touchio.TouchIn(A3)
@@ -24,82 +23,80 @@ touch4 = touchio.TouchIn(A4)
 touch5 = touchio.TouchIn(A5)
 touch6 = touchio.TouchIn(A6)
 touch7 = touchio.TouchIn(A7)
-slide_switch = digitalio.DigitalInOut(board.SLIDE_SWITCH)
-slide_switch.switch_to_input(pull=digitalio.Pull.UP)
 sample = audioio.AudioOut(board.SPEAKER, sine_wave)
 
 while True:
-    if slide_switch.value:
+    if circuit.switch:
         print("Slide switch off!")
-        pixels.fill((0, 0, 0))
-        pixels.write()
+        circuit.pixels.fill((0, 0, 0))
+        circuit.pixels.write()
         sample.stop()
         continue
     if touch1.value:
         print('touched 1!')
-        pixels.fill((51, 0, 0))
-        pixels.write()
+        circuit.pixels.fill((30, 0, 0))
+        circuit.pixels.write()
         if not sample.playing or (
           (sample.playing and not sample.frequency == 8000)):
             sample.frequency = 8000
             sample.play(loop=True)
     elif touch2.value:
         print('touched 2!')
-        pixels.fill((51, 25, 0))
-        pixels.write()
+        circuit.pixels.fill((30, 10, 0))
+        circuit.pixels.write()
         if not sample.playing or (
          (sample.playing and not sample.frequency == 9000)):
             sample.frequency = 9000
             sample.play(loop=True)
     elif touch3.value:
         print('touched 3!')
-        pixels.fill((51, 51, 0))
-        pixels.write()
+        circuit.pixels.fill((30, 30, 0))
+        circuit.pixels.write()
         if not sample.playing or (
          (sample.playing and not sample.frequency == 10000)):
             sample.frequency = 10000
             sample.play(loop=True)
     elif touch4.value:
         print('touched 4!')
-        pixels.fill((0, 51, 0))
-        pixels.write()
+        circuit.pixels.fill((0, 30, 0))
+        circuit.pixels.write()
         if not sample.playing or (
          (sample.playing and not sample.frequency == 10800)):
             sample.frequency = 10800
             sample.play(loop=True)
     elif touch5.value:
         print('touched 5!')
-        pixels.fill((0, 51, 51))
-        pixels.write()
+        circuit.pixels.fill((0, 30, 30))
+        circuit.pixels.write()
         if not sample.playing or (
          (sample.playing and not sample.frequency == 12000)):
             sample.frequency = 12000
             sample.play(loop=True)
     elif touch6.value and not touch7.value:
         print('touched 6!')
-        pixels.fill((0, 0, 51))
-        pixels.write()
+        circuit.pixels.fill((0, 0, 30))
+        circuit.pixels.write()
         if not sample.playing or (
          (sample.playing and not sample.frequency == 13500)):
             sample.frequency = 13500
             sample.play(loop=True)
     elif touch7.value and not touch6.value:
         print('touched 7!')
-        pixels.fill((25, 0, 51))
-        pixels.write()
+        circuit.pixels.fill((10, 0, 30))
+        circuit.pixels.write()
         if not sample.playing or (
          (sample.playing and not sample.frequency == 15000)):
             sample.frequency = 15000
             sample.play(loop=True)
     elif touch6.value and touch7.value:
         print('touched 8!')
-        pixels.fill((51, 0, 51))
-        pixels.write()
+        circuit.pixels.fill((30, 0, 30))
+        circuit.pixels.write()
         if not sample.playing or (
          (sample.playing and not sample.frequency == 16000)):
             sample.frequency = 16000
             sample.play(loop=True)
     else:
-        pixels.fill((0, 0, 0))
-        pixels.write()
+        circuit.pixels.fill((0, 0, 0))
+        circuit.pixels.write()
         sample.stop()
